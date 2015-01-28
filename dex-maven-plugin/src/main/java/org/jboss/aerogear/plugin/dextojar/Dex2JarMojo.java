60f538abe22fc149871402072eb75724db1466fd
Summers Pittman <secondsun@gmail.com>
2015-01-28 17:18:44 -0500

    Create Mojo

--- dex-maven-plugin/src/main/java/org/jboss/aerogear/plugin/dextojar/Dex2JarMojo.java
+++ dex-maven-plugin/src/main/java/org/jboss/aerogear/plugin/dextojar/Dex2JarMojo.java
@@ -1,8 +1,19 @@
 package org.jboss.aerogear.plugin.dextojar;
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
 /**
  * This will explode a aar, extract the dex file, and create a jar.
  */
-public class Dex2JarMojo {
+@Mojo( name = "dex2jar")
+public class Dex2JarMojo extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
