905f5a3f8bf2e72d0a97e9eaaee97f4850f1a6d6
Summers Pittman <secondsun@gmail.com>
2015-01-30 12:53:24 -0500

    Initial work towards dex2jar maven plugin

--- dex-maven-plugin/src/test/java/org/jboss/aerogear/plugin/dex2jar/Dex2JarMojoTest.java
+++ dex-maven-plugin/src/test/java/org/jboss/aerogear/plugin/dex2jar/Dex2JarMojoTest.java
@@ -2,7 +2,7 @@ package org.jboss.aerogear.plugin.dex2jar;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 
-public class Dex2JarMojoTests extends AbstractMojoTestCase {
+public class Dex2JarMojoTest extends AbstractMojoTestCase {
 
     public void testEmptyFileThrowsException() {
         fail("not implemented");
